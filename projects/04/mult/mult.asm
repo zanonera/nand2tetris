32d139cc4b74e33dacab65c7d0c9efb053026a8e
Ricardo Zanone <ricardo.zanone@atmossistemas.com.br>
2019-03-26 17:34:10 -0300

    Added project4 Mult initial conditional checking

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,24 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//Clear RAM[2] before everything
+	@0
+	D=A
+	@R2
+	M=D
+
+//IF R1 or R0 is equal to zero, goto the end
+	@R0
+	D=M		// D=RAM[0]
+	@END
+	D;JEQ	// If R0=0 goto END
+
+	@R1
+	D=M		// D=RAM[1]
+	@END
+	D;JEQ	// If R1=0 goto END
+	
+(END)
+	@END
+	0;JMP
