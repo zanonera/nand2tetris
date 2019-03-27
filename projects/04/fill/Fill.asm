2ef438f111a29f8a5d9c4bcf4faa9006a27ccc19
zanonera <ricardo_zanone@yahoo.com.br>
2019-03-26 23:22:00 -0300

    project4 fill.asm with a preliminar code that fill the screen totally in black

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,42 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// clear the loop iterator var
+@i
+M=0
+
+// start the var pos with the screen base address
+@SCREEN
+D=A
+@pos
+M=D
+
+// n = the total size of the video ram
+@8192
+D=A
+@n
+M=D
+
+(LOOP)
+	@i
+	D=M
+	@n
+	D=D-M
+	@END
+	D;JEQ // if i == n goto STOP
+	@pos
+	D=M
+	@i
+	A=D+M
+	M=-1
+	@pos
+	D=D+1
+	@i
+	M=M+1 // i = i + 1
+	@LOOP
+	0;JMP
+	
+(END)
+	@END
+	0;JMP 
